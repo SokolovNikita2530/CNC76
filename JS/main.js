53e19b1ba490c76e25c5f63db272187870885155
SokolovNikita2530 <64984157+SokolovNikita2530@users.noreply.github.com>
2020-12-25 20:38:33 +0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function() {
   		};
 	};
 
-	let stop;
+	let stop = true;
 
 	function stopAnimate() {
 		if(document.body.clientWidth >= 1440) {
@@ -46,8 +46,16 @@ document.addEventListener('DOMContentLoaded', function() {
 		}
 	}
 
-	stopAnimate();
-	animateBlocks();
+	if(document.body.clientWidth >= 1440) {
+		animateBlocks();
+		stop = true;
+	} else if (document.body.clientWidth < 1440) {
+		stop = false;
+		let collapse = document.getElementsByClassName('navbar-collapse')[0];
+		if (collapse.classList.contains('animate__animated')) {
+			collapse.classList.remove('animate__animated', 'animate__fadeInDown');
+		}
+	}
 
 	function animateBlocks() {
 		let fadeLeft = document.getElementsByClassName("fadeLeft");
